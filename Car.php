13f35a893af40a61b6f8c960b7230f93c274218a
Levend <levend-mohammad_student2020@wilder.school>
2020-04-18 17:30:45 +0200

    Create Car.php

---
+++ Car.php
@@ -0,0 +1,36 @@
+
+class Car extends Vehicle{
+    private $hasParkBrake;
+
+    //public function __construct($color)
+    //{
+    //  parent::__construct($color);
+    //}
+
+
+    public function start()
+    {
+
+        if($this->hasParkBrake){
+            throw new Exception("Le freinà main est mis");
+        }
+
+        return "I'm start !";
+    }
+
+    public function getHasParkBrake()
+    {
+        return $this->hasParkBrake;
+    }
+    
+    
+    public function setHasParkBrake(bool $hasParkBrake)
+    {
+        $this->hasParkBrake = $hasParkBrake;
+
+        return $this;
+    }
+
+
+
+}
